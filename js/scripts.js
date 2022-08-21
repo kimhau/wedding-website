588e832cb96be5ffab516819d22373e0afc0664c
unknown <mimikim3718@gmail.com>
2022-08-22 01:08:42 +0800

    update

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzMg-FdgVvILvpvmwzcQBf_MLmItVdNXl6Ro_-NRjPjm5x1b9rKIYbNRh2_QbLGgF5P/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbz3M23qxj0BqJq7S1xhsrO4aRBm7dqtiUQ6aHpRwpd15-IgJCyXl07iq6ChAj0MaFhU/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
