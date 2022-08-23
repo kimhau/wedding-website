316972e58a01919e79c2dfcec17ede1169a2b28b
unknown <mimikim3718@gmail.com>
2022-08-23 14:48:27 +0800

    add nuan nuan mp3

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyE3VhPD_D-7GIzqA5a6FHn8S4BVdIunZTmqMu8zEOZnzaN8zPBe-G_lw8YvSrKx9oT/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycby_-bAeJXiMzHL8HwYZVAqndaBsu9tRt8cQH3eb-7TY_rWvl7dx_3vKfey9vmeF3ihb/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
